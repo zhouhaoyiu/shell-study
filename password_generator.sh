e51f944e178fbdb632e2576a8a3f44d4dd21b1c1
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2022-05-08 22:31:19 +0800

    some changes
    Signed-off-by: zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>

--- password_generator.sh
+++ password_generator.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 flag=false
+easy=false
 for i in $*
 do
-  if [ ${i} == "-w" ]
+  if [ ${i} == "-s" ]
   then
-  echo "Warning: password will be saved in log/password.txt"    
-  flag=true
+      echo -e '\033[47;30m'"Warning: password will be saved in log/password.txt"'\033[0m'
+      flag=true
+  fi
+  if [ ${i} == "-e" ]
+  then
+      echo -e '\033[41;37m'Info: I will generate an easy password '\033[0m'
+      easy=true
   fi
 done
 #随机生成n位随机密码
   
 #定义密码取值范围
-String="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-echo "请输入要生成的密码位数"
+if [ $easy = true ]
+then
+    String="0123456789abc"
+else
+    String="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+fi
+echo "请输入要生成的密码位数(0到20)"
 read -p "位数：" n
-echo "请输入密码用途"
+if [ ${n} -le 20 ] && [ ${n} -gt 0 ]
+then
+    echo "请输入密码用途"
+else
+    echo "输入错误"
+    exit 0
+fi
 read -p "用途：" usage
+
+echo -e '\033[0;42m'  Password Generating  '\033[0m'
 #循环n次
 for ((i=0;i<n;i++))
 do
@@ -23,8 +42,12 @@ do
     num=$[$RANDOM%${#String}]
     passwd=$passwd${String:$num:1}
 done
-echo "您的密码是：$passwd"
+echo "您的密码是：$passwd, 用途：$usage"
 if [ ${flag} == true ]
 then
+  if [ ! -d log ]
+  then
+    mkdir log
+  fi
   echo "password：$passwd length: $n used by $usage created $(date "+%Y-%m-%d %H:%M:%S")" >> log/password.txt
 fi
